5d43320880b1dd25cded34c04c82f8bdd4352a4f
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-03-11 18:31:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-
+let yourArray. = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    },
+	  {
+		fail: false;
+		pass: true
+	  }
+  ]
+];
+console.log(yourArray);
